7f36b045961d4a7e77581f7b4805801a6f1cbc07
Keviinraj BALARATNAM <keviinraj.balaratnam@gmail.com>
2019-12-19 16:40:28 +0100

    fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   console.log('Hello world received a request.');
 
   const target = process.env.TARGET || 'World';
-  res.send(`Dream ${target}!`);
+  res.send(`YOLO ${target}!`);
 });
 
 const port = process.env.PORT || 8080;
