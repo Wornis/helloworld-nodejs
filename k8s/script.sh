845035cfa003327845e68502f3745b9496f0ed07
Kéviinraj BALARATNAM <keviinraj.balaratnam@gmail.com>
2019-12-20 00:07:55 +0100

    fix

--- k8s/script.sh
+++ k8s/script.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-sed -e 's/$TAG/'"$1"'/g' k8s/deployment.yaml > /workspace/deployment_production.yaml
+mkdir /workspace
+sed -e 's/$TAG/'"$1"'/g' k8s/deployment.yaml > deployment_production.yaml
