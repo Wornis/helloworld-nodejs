2996594689978613d78b897182639111092d91c4
Keviinraj BALARATNAM <keviinraj.balaratnam@gmail.com>
2019-12-20 14:46:05 +0100

    remvoe console

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  console.log('Hello KEvin.');
-
   const target = process.env.TARGET || 'World';
   res.send(`Salut BRother ${target}!`);
 });
