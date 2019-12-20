71544c10abd26fd6370b8439f868c12d31ed4a14
Keviinraj BALARATNAM <keviinraj.balaratnam@gmail.com>
2019-12-20 14:35:53 +0100

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  console.log('Hello world received a request.');
+  console.log('Hello KEvin.');
 
   const target = process.env.TARGET || 'World';
   res.send(`Salut BRother ${target}!`);
